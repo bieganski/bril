e869effb5ef183571e95fe48c2ef693a6035c07e
Matuesz <bieganski.m@wp.pl>
2023-03-14 16:08:46 +0100

    have <<something>> working, with dead code elim.

--- mateusz/lec3/dead_code_removal.py
+++ mateusz/lec3/dead_code_removal.py
@@ -4,8 +4,28 @@ import json
 import sys
 
 def main(j):
-    print(j)
+    for f in j["functions"]:
+
+        # Find all globally used symbols.
+        used = []
+        for ins in f["instrs"]:
+            args = ins.get("args") or []
+            used.extend(args)
+
+        # Remove assigned symbols that we know about that are globally unused.
+        rms = []
+        for i, ins in enumerate(f["instrs"]):
+            dest = ins.get("dest")
+            if dest and dest not in used:
+                rms.append(i)
+        
+        for idx in rms:
+            f["instrs"].pop(idx)
+
 
 if __name__ == "__main__":
-    j = json.load(sys.stdin)
+    text = str(sys.stdin.read())
+    # print(text)
+    j = json.loads(text)
     main(j)
+    json.dump(j, sys.stdout)
