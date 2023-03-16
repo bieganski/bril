99b99338417a8d0d37e011262cb5d807792d8234
Matuesz <bieganski.m@wp.pl>
2023-03-16 10:13:37 +0100

    lvn stub

--- mateusz/lec3/lvn.py
+++ mateusz/lec3/lvn.py
@@ -5,8 +5,35 @@ import sys
 
 from dead_code_removal import main as DCR
 
-def main(j):
-    DCR(j)
+from dataclasses import dataclass
+
+@dataclass
+class State:
+    pass
+
+def create_lvn_mapping(s: State, j: dict):
+    """
+    @param 'j' is a subdict, only for a function or basic block (it needs to have 'instrs' as a direct child!)
+    """
+    for ins in j["instrs"]:
+        raise ValueError(ins)
+
+def reassign_code_inplace(s: State, j: dict):
+    """
+    @param 'j' is a subdict, only for a function or basic block (it needs to have 'instrs' as a direct child!)
+    """
+    pass
+
+def main(j: dict):
+    """
+    Computes LVN, modifies code in a way that applies lvn-related optimizations.
+    """
+    
+    for f in j["functions"]:
+        s = State()
+        jj = f["instrs"]
+        create_lvn_mapping(   s=s, j=jj)
+        reassign_code_inplace(s=s, j=jj)
     
 
 if __name__ == "__main__":
