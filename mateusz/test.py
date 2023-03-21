00f421ea07be3e5800aa8065bc9da3ce01adb7d5
Matuesz <bieganski.m@wp.pl>
2023-03-21 13:30:36 +0100

    generalize mateusz/test.py, to be used not only for lvn.

--- mateusz/test.py
+++ mateusz/test.py
@@ -3,10 +3,17 @@
 import glob
 import subprocess
 from pathlib import Path
+from enum import Enum
+from argparse import ArgumentParser
 
 from ikva_utils.misc.utils import get_color_logging_object, get_git_root
 
 logging = get_color_logging_object()
+
+class TestMode(Enum):
+    LVN = "lvn"
+    DF = "df"
+    
 
 # from logging import CRITICAL
 # logging.setLevel(CRITICAL)  # surpress most of output.
@@ -16,20 +23,28 @@ blacklist = [
     "nonlocal.bril",
 ]
 
-def main(*args):
-    top_dir = Path(__file__).parent.parent
+OPT_PASS = {
+    TestMode.LVN: "mateusz/lec3/lvn.py",
+    TestMode.DF: "mateusz/lec4/df.py",
+}
+
+def main(files: list[Path], mode: TestMode):
     
-    if len(args):
+    if len(files):
         # handle command-line specified tests..
-        for x in args:
-            assert x.endswith(".bril"), x
-        tests = args
+        for x in files:
+            assert x.suffix == ".bril", x
+            assert x.exists(), x
+        tests = files
     else:
         # ..or fallback to default tests.
+        top_dir = Path(__file__).parent.parent
         tests_dir = top_dir / "examples" / "test" / "lvn"
-        tests = glob.glob(str(tests_dir / "*.bril"))
+        files = glob.glob(str(tests_dir / "*.bril"))
 
-    tests = [Path(x) for x in tests]
+    tests = [Path(x) for x in files]
+
+    opt_pass = get_git_root() / OPT_PASS[mode]
     
     for x in tests:
 
@@ -46,13 +61,13 @@ def main(*args):
                 print(stderr.decode("ascii"))
                 raise ValueError(f"command '{cmd}' returned {p.returncode}")
             # expected stderr format:
-            # total_dyn_inst: 5
+            # 'total_dyn_inst: 5\n'
             instructions_executed = int(stderr.splitlines()[0].split()[-1])
             ret_value = stdout
             return instructions_executed, ret_value
 
         ie, ret = run_and_parse(f"set -o pipefail; cat {x} | bril2json | brili -p")
-        opt_pass = get_git_root() / "mateusz/lec3/lvn.py"
+        
         y = Path(x).with_suffix(".mtk")
         _ie, _ret = run_and_parse(f"set -o pipefail; cat {x} | bril2json | {opt_pass} | tee wtf | bril2txt | tee {y} | bril2json | brili -p")
 
@@ -80,6 +95,10 @@ def main(*args):
         if _ie < ie:
             logging.critical(f"Executed {_ie} instructions instead of {ie}! nice work bro")
 
+
 if __name__ == "__main__":
-    import sys
-    main(*sys.argv[1:])
+    p = ArgumentParser()
+    p.add_argument("--files", nargs="+", type=Path, default=[])
+    p.add_argument("--mode", type=TestMode, required=True, choices=[x for x in TestMode])
+
+    main(**vars(p.parse_args()))
